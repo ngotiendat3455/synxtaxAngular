66106ba75f12e690a4b51246e32f6a5d73e9ce1e
ngotiendat3455 <datngotien345@gmail.com>
2024-04-28 00:49:34 +0700

    http api requests to dump server

---
+++ http-01-start/src/app/app.component.ts
@@ -0,0 +1,112 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { Product } from './post.model';
+import { ProductService } from './posts.service';
+
+// import { Post } from './post.model';
+// import { PostsService } from './posts.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  loadedPosts: Product[] = [];
+  isFetching: boolean = false;
+  isError: string = null;
+  // firebase_url: string =
+  //   'https://ng-complete-guide-a2859-default-rtdb.firebaseio.com/';
+
+  constructor(private http: HttpClient, private postService: ProductService) { }
+
+  ngOnInit() {
+    this.postServiceSubscribe();
+  }
+
+  // send and store json object to FireBase
+  // 'posts.json' is the file directory
+  // -----------------------------------------------------------------------
+  onCreatePost(postData: any) {
+    // Send Http request
+    this.postService.createAndStoreProduct(postData.title, postData.content);
+    console.log(postData);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.isFetching = true;
+    this.postServiceSubscribe();
+  }
+
+  // subscribes to the observable in the postService file
+  // encapsulated method to reduce duplication
+  private postServiceSubscribe() {
+    this.postService.fetchProducts().subscribe(
+      {
+        next: (v) => {
+          this.isFetching = false;
+          this.loadedPosts = v.products;
+          console.log(v);
+        },
+        error: (e) => console.error(e),
+        complete: () => console.info('complete')
+      }
+      // (posts) => {
+      //   this.isFetching = false;
+      //   this.loadedPosts = posts;
+      // },
+      // (error) => {
+      //   this.isError = error.message;
+      // }
+    );
+  }
+
+  onClearPosts(id = 1) {
+    // Send Http request
+    this.postService.deleteProduct(id).subscribe(
+      {
+        next: (v) => {
+          this.loadedPosts = [];
+          console.log('All posts deleted successfully.');
+        },
+        error: (e) => console.error('Error deleting all posts:', e),
+        complete: () => console.info('complete')
+      }
+      // () => {
+      //   this.loadedPosts = [];
+      //   console.log('All posts deleted successfully.');
+      // },
+      // (error) => {
+      //   console.error('Error deleting all posts:', error);
+      // }
+    );
+  }
+
+  // get the json data from FireBase
+  // automatically store created id in the posted object as a array
+  // subscribe and log to console
+  // only knows this is an object but not what the object looks like
+  // MOVED INTO -> posts.service.ts
+  // -----------------------------------------------------------------------
+  // private fetchPosts() {
+  //   this.http
+  //     .get(this.firebase_url + 'posts.json')
+  //     .pipe(
+  //       map((responseData) => {
+  //         const postsArray: Post[] = [];
+  //         for (const key in responseData) {
+  //           if (responseData.hasOwnProperty(key)) {
+  //             postsArray.push({ ...responseData[key], id: key });
+  //           }
+  //         }
+  //         return postsArray;
+  //       })
+  //     )
+  //     .subscribe((requestData) => {
+  //       console.log(requestData);
+  //       this.loadedPosts = requestData;
+  //     });
+  // }
+}
---
+++ http-01-start/src/app/posts.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Post, Product } from './post.model';
+import { debounce, map, timer } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  base_url: string =
+    'https://dummyjson.com/';
+
+  // doesn't care about the response/ response status
+  createAndStoreProduct(title: string, content: string) {
+    const postData: Product =  {
+      "title": title,
+      "description": content,
+      "price": 549,
+      "discountPercentage": 12.96,
+      "rating": 4.69,
+      "stock": 94,
+      "brand": "Apple",
+      "category": "smartphones",
+      "thumbnail": "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+      "images": [
+      ]
+    };
+    this.http.post<any>(this.base_url + 'products/add', postData).subscribe((responseData) => {
+      console.log(responseData);
+    })
+  }
+
+  fetchProducts() {
+    // returns an observable to subscribe to in component file
+    // doe care about the response/ response status so subscription is done outside of file
+    return this.http
+      .get<any>(this.base_url + 'products')
+      .pipe(
+        debounce(() => timer(2000))
+      );
+    // going to use subjects here.
+    // since this will be a subject no need to subscribe here
+    // the "subscription/ listening will be done in -> app.component.ts"
+    // .subscribe((requestData) => {
+    //   // console.log(requestData);
+    // });
+  }
+
+  deleteProduct(id: number) {
+    const deleteUrl = `${this.base_url}products/${id}`;
+
+    return this.http.delete(deleteUrl);
+  }
+
+  constructor(private http: HttpClient) {}
+}
