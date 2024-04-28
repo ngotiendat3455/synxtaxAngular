574f2670ba84516ee16598e3e14e112f445f1be1
ngotiendat3455 <datngotien345@gmail.com>
2024-04-28 19:52:38 +0700

    sign up works

---
+++ Recipe_Book/src/app/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+interface AuthResponseData {
+  // kind: string; //not shown in api docs for some reason
+  idToken: string; // A Firebase Auth ID token for the newly created user.
+  email: string; // The email for the newly created user.
+  refreshToken: string; // A Firebase Auth refresh token for the newly created user.
+  expiresIn: string; //The number of seconds in which the ID token expires.
+  localId: string; // The uid of the newly created user.
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  // signs up and logs in to FireBase
+  postKey = 'https://dummyjson.com/users';
+
+  // apiKey = 'AIzaSyBNUvsG5N7mhCGLn5iJOla5J4mHUHsUJfA';
+
+  signUp(email: string, pswrd: string) {
+    return this.http.post<AuthResponseData>(this.postKey + '/add', {
+      email: email,
+      password: pswrd,
+      firstName: 'test',
+      lastName: 'test',
+      age: 12,
+    });
+  }
+
+  constructor(private http: HttpClient) {}
+}
--- Recipe_Book/src/app/auth/auth/auth.component.ts
+++ Recipe_Book/src/app/auth/auth/auth.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
+  loginMode = false;
 
+  @ViewChild('f', { static: false }) authForm: NgForm;
+
+  constructor(private authService: AuthService) {}
+
+  switchMode() {
+    this.loginMode = !this.loginMode;
+  }
+
+  onSubmit(form: NgForm){
+    const email = form.value.email;
+    const pswrd = form.value.password;
+    console.log(form.value);
+
+    if (this.loginMode) {
+    } else {
+      this.authService.signUp(email, pswrd).subscribe(
+        (resData) => {
+          console.log(resData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+
+    form.reset();
+  }
 }
