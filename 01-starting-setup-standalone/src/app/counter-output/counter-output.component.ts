62374741bce35c7819c393eea8ae211636ccf944
ngotiendat3455 <datngotien345@gmail.com>
2024-04-30 21:07:59 +0700

    use of selectors

--- 01-starting-setup-standalone/src/app/counter-output/counter-output.component.ts
+++ 01-starting-setup-standalone/src/app/counter-output/counter-output.component.ts
@@ -4,6 +4,7 @@ import { Observable, Subscription } from 'rxjs';
 import { CounterService } from '../counter.service';
 import { Store } from '@ngrx/store';
 import { AsyncPipe } from '@angular/common';
+import { doubleCount, selectCount } from '../counter-store/counter.selector';
 
 @Component({
   selector: 'app-counter-output',
@@ -16,13 +17,15 @@ export class CounterOutputComponent implements OnInit, OnDestroy {
   // counter = 0;
   // counterServiceSub?: Subscription;
   count$!: Observable<number>; 
+  double$!: Observable<number>;
   constructor(private store: Store<{ counter: number }>) {}
 
   ngOnInit(): void {
     // this.counterServiceSub = this.counterService.counterChanged.subscribe(
     //   (newVal) => (this.counter = newVal)
     // );
-    this.count$ = this.store.select('counter');
+    this.count$ = this.store.select(selectCount);
+    this.double$ = this.store.select(doubleCount);
   }
 
   ngOnDestroy(): void {
--- 01-starting-setup-standalone/src/app/counter-store/counter.actions.ts
+++ 01-starting-setup-standalone/src/app/counter-store/counter.actions.ts
@@ -1,4 +1,4 @@
 import { createAction, props } from '@ngrx/store';
 
 export const increment = createAction('[Counter] increment', props<{ value: number }>());
-export const decrement = createAction('[Counter] decrement');
+export const decrement = createAction('[Counter] decrement', props<{ value: number }>());
