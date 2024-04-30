e1028f4d565082f90c958da483de3694b462f67e
ngotiendat3455 <datngotien345@gmail.com>
2024-04-30 20:25:34 +0700

    try an older method of creating reducers

--- 01-starting-setup-standalone/src/app/counter-store/counter.reducer.ts
+++ 01-starting-setup-standalone/src/app/counter-store/counter.reducer.ts
@@ -8,3 +8,10 @@ export const counterReducer = createReducer(
     on(increment, (state, action) => state + action.value), // listen to an action and define what happens on when the action is called
     on(decrement, (state) => state - 1)
   );
+
+// export function counterReducer(state = initialState, action: any) {
+//   if (action.type === '[Counter] increment') {
+//     return state + action.value;
+//   }
+//   return state;
+// }
